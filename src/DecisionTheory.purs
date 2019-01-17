cafab986ce84f5a0bd0a39e2fe64c66ccce9cd38
Cole Haus <colehaus@cryptolab.net>
2019-01-17 12:00:05 -0800

    Switch to NonEmptyList
    Dominating without any underlying states doesn't make sense

--- src/DecisionTheory.purs
+++ src/DecisionTheory.purs
@@ -3,9 +3,9 @@ module DecisionTheory where
 import Prelude hiding ((>=), (>))
 
 import Data.Foldable as Foldable
-import Data.List (List)
-import Data.List as List
 import Data.Maybe (Maybe(..))
+import Data.List.NonEmpty (NonEmptyList)
+import Data.List.NonEmpty as NonEmpty
 import Data.Ord.Partial (class PartialOrd, (>=), (>))
 import Data.Tuple (Tuple(..))
 import Data.Table (Table)
@@ -14,21 +14,23 @@ import Data.Table as Table
 dominatesWeakly ::
   forall rowId columnId cell column.
   Eq rowId => PartialOrd cell =>
-  Table rowId columnId cell (List cell) column -> rowId -> rowId -> Maybe Boolean
+  Table rowId columnId cell (NonEmptyList cell) column -> rowId -> rowId -> Maybe Boolean
 dominatesWeakly t rId1 rId2 =
   case Tuple (Table.row t rId1) (Table.row t rId2) of
     Tuple Nothing _ -> Nothing
     Tuple _ Nothing -> Nothing
     Tuple (Just r1) (Just r2) ->
-      Just <<< Foldable.all ((==) true) $ List.zipWith (>=) r1 r2
+      Just <<< Foldable.all ((==) true) $ NonEmpty.zipWith (>=) r1 r2
 
 dominatesStrongly ::
   forall rowId columnId cell column.
   Eq rowId => PartialOrd cell =>
-  Table rowId columnId cell (List cell) column -> rowId -> rowId -> Maybe Boolean
+  Table rowId columnId cell (NonEmptyList cell) column -> rowId -> rowId -> Maybe Boolean
 dominatesStrongly t rId1 rId2 =
   case Tuple (Table.row t rId1) (Table.row t rId2) of
     Tuple Nothing _ -> Nothing
     Tuple _ Nothing -> Nothing
     Tuple (Just r1) (Just r2) ->
-      Just $ Foldable.all ((==) true) (List.zipWith (>=) r1 r2) && Foldable.any ((==) true) (List.zipWith (>) r1 r2)
+      Just $
+        Foldable.all ((==) true) (NonEmpty.zipWith (>=) r1 r2) &&
+        Foldable.any ((==) true) (NonEmpty.zipWith (>) r1 r2)
--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -6,7 +6,6 @@ import Data.Either (Either)
 import Data.Either as Either
 import Data.List (List)
 import Data.List.NonEmpty (NonEmptyList)
-import Data.List.NonEmpty as NEList
 import Data.Map as Map
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
@@ -24,9 +23,6 @@ import DecisionTheory
 fromRightEx :: forall l r. Either l r -> r
 fromRightEx x = unsafePartial $ Either.fromRight x
 
-mk :: forall a. NonEmptyList a -> Maybe (List a)
-mk = Just <<< NEList.toList
-
 main :: Effect Unit
 main = run [consoleReporter] do
   describe "Domination" do
@@ -38,7 +34,7 @@ main = run [consoleReporter] do
               , Tuple (Tuple "row2" "column1") 1
               , Tuple (Tuple "row2" "column2") 1
               ]
-            table = fromRightEx $ Table.mk mk mk (Map.fromFoldable cells)
+            table = fromRightEx $ Table.mk Just Just (Map.fromFoldable cells)
         dominatesWeakly table "row1" "row2" `shouldEqual` Just true
       it "returns `Just false` for non-domination" do
         let cells =
@@ -47,7 +43,7 @@ main = run [consoleReporter] do
               , Tuple (Tuple "row2" "column1") 9
               , Tuple (Tuple "row2" "column2") 9
               ]
-            table = fromRightEx $ Table.mk mk mk (Map.fromFoldable cells)
+            table = fromRightEx $ Table.mk Just Just (Map.fromFoldable cells)
         dominatesWeakly table "row1" "row2" `shouldEqual` Just false
       it "returns `Nothing` for missing rows" do
         let cells =
@@ -56,7 +52,7 @@ main = run [consoleReporter] do
               , Tuple (Tuple "row2" "column1") 1
               , Tuple (Tuple "row2" "column2") 1
               ]
-            table = fromRightEx $ Table.mk mk mk (Map.fromFoldable cells)
+            table = fromRightEx $ Table.mk Just Just (Map.fromFoldable cells)
         dominatesWeakly table "row1" "row3" `shouldEqual` Nothing
     describe "Strong" do
       it "returns `Just true` for domination" do
@@ -66,7 +62,7 @@ main = run [consoleReporter] do
               , Tuple (Tuple "row2" "column1") 1
               , Tuple (Tuple "row2" "column2") 1
               ]
-            table = fromRightEx $ Table.mk mk mk (Map.fromFoldable cells)
+            table = fromRightEx $ Table.mk Just Just (Map.fromFoldable cells)
         dominatesStrongly table "row1" "row2" `shouldEqual` Just true
       it "returns `Just false` for non-domination" do
         let cells =
@@ -75,7 +71,7 @@ main = run [consoleReporter] do
               , Tuple (Tuple "row2" "column1") 1
               , Tuple (Tuple "row2" "column2") 1
               ]
-            table = fromRightEx $ Table.mk mk mk (Map.fromFoldable cells)
+            table = fromRightEx $ Table.mk Just Just (Map.fromFoldable cells)
         dominatesStrongly table "row1" "row2" `shouldEqual` Just false
       it "returns `Nothing` for missing rows" do
         let cells =
@@ -84,5 +80,5 @@ main = run [consoleReporter] do
               , Tuple (Tuple "row2" "column1") 1
               , Tuple (Tuple "row2" "column2") 1
               ]
-            table = fromRightEx $ Table.mk mk mk (Map.fromFoldable cells)
+            table = fromRightEx $ Table.mk Just Just (Map.fromFoldable cells)
         dominatesStrongly table "row1" "row3" `shouldEqual` Nothing
